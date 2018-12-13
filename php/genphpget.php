75f9d621b96a9799ef8c5268c951b7847480f136
Fernando Paniagua <fernando.paniagua@juanxxiii.net>
2018-12-13 17:18:44 +0100

    Initial commit

---
+++ php/genphpget.php
@@ -0,0 +1,4 @@
+<?php
+    $nombre = $_GET["nombre"];
+    $edad = $_GET["edad"];
+    echo "(Version GET) Te llamas " . $nombre . " y tienes " . $edad . " años";
---
+++ php/genphppost.php
@@ -0,0 +1,10 @@
+<?php
+    $nombre = $_POST["nombre"];
+    $edad = $_POST["edad"];
+    echo "Te llamas " . $nombre . " y tienes " . $edad . " años";
+
+
+
+
+
+
